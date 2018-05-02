db0676605b8536cd66d3f2228c00b2e77548d7ac
Elodie Prévôt <eprevot@worldia.com>
2018-05-02 22:39:29 +0200

    improve category badge and results list integration

--- src/search.js
+++ src/search.js
@@ -20,17 +20,17 @@ search.addWidget(
 );
 
 var hitTemplate =
-  '<div class="hit media">' +
-    //'<div class="media-left">' +
-      //'<div class="media-object" style="background-image: url(\'{{image}}\');"></div>' +
-    //'</div>' +
-    '<div class="media-body">' +
-      '<h4 class="media-heading">{{{_highlightResult.name.value}}}</h4>' +
-      '<p class="genre">{{description}}</p>' +
-      '<p><audio src="{{soundUrl}}" type="audio/wav" preload="none" controls="">Your browser does not support the audio element.</audio>' +
-      '<a class="btn btn-primary" download="" href="{{soundUrl}}" aria-label="{{name}}">Download</a></p>' +
-    '</div>' +
-  '</div>';
+'<table class="table table-hover">' +
+  '<tbody>' +
+  '{{#hits}}' +
+    '<tr>' +
+      '<td>{{description}}</td>' +
+      '<td><audio src="{{soundUrl}}" type="audio/wav" preload="none" controls="">Your browser does not support the audio element.</audio></td>' +
+      '<td><a class="btn btn-primary" download="" href="{{soundUrl}}" aria-label="{{name}}">Download</a></td>' +
+    '</tr>' +
+  '{{/hits}}' +
+  '</tbody>' +
+'</table>'
 
 var noResultsTemplate =
   '<div class="text-center">No results found matching <strong>{{query}}</strong>.</div>';
@@ -41,17 +41,8 @@ search.addWidget(
     hitsPerPage: 10,
     templates: {
       empty: noResultsTemplate,
-      item: hitTemplate
+      allItems: hitTemplate
     },
-    /*
-     *transformData: function(hit) {
-     *  hit.stars = [];
-     *  for (var i = 1; i <= 5; ++i) {
-     *    hit.stars.push(i <= hit.rating);
-     *  }
-     *  return hit;
-     *}
-     */
   })
 );
 
@@ -69,11 +60,7 @@ search.addWidget(
   instantsearch.widgets.hierarchicalMenu({
     container: '#categories',
     attributes: ['categories.lvl0', 'categories.lvl1', 'categories.lvl2', 'categories.lvl3'],
-    templates: {
-      header: 'Categories'
-    },
     cssClasses: {
-      list: 'nav nav-list',
       count: 'badge pull-right',
       active: 'active'
     },
@@ -91,21 +78,5 @@ search.addWidget(
     clearsQuery: true,
   })
 )
-
-/*
- *search.addWidget(
- *  instantsearch.widgets.refinementList({
- *    container: '#categories',
- *    attributeName: 'categories.lvl0',
- *    operator: 'and',
- *    limit: 1000,
- *    cssClasses: {
- *      list: 'nav nav-list',
- *      count: 'badge pull-right',
- *      active: 'active'
- *    }
- *  })
- *);
- */
 
 search.start();
